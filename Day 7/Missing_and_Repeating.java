d096346cc98a6e089dc181d3773b6bb8170313b8
mus-muskan <muskan.xyz1151@gmail.com>
2022-10-19 18:07:42 +0530

    Day 7 Q2 All Test Cases Passed

--- Day 7/Missing_and_Repeating.java
+++ Day 7/Missing_and_Repeating.java
@@ -7,14 +7,16 @@ public class Solution {
     public static int[] missingAndRepeating(ArrayList<Integer> arr, int n) {
         int s=(n*(n+1))/2;
         Collections.sort(arr);
-        int i=0 , j=n , k=0 , sum=0, ans[]= new int[2];
+        int i=0 , k=0 , sum=0, ans[]= new int[2];
         for (k=0 ; k<n ; k++)
             sum+=arr.get(k);
-        while(i<n)
+        for(i=1 ; i<=n ;i++)
         {
-            if((arr.get(i))!=i+1)
-            ans[0]=i+1;
-            i++;
+            if(!(arr.contains(i))) //Changed the function to find the missing element
+            { 
+                ans[0]=i;
+                break;
+            }
            
         }  
          ans[1]=sum+ans[0]-s;
